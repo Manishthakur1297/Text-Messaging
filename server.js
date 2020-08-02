69473d1f539d3b7f6abbe076f1e823fad8fd220a
Manish Thakur <manishthakur1297@gmail.com>
2020-08-03 04:42:36 +0530

    Herorku deploy

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 const connectDB = require("./config/db");
 
@@ -29,6 +30,10 @@ app.use("/api/posts", require("./routes/api/posts"));
 if (process.env.NODE_ENV === "production") {
   // Set static folder
   app.use(express.static(path.join(__dirname, "client/build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 
 const PORT = process.env.PORT || 5000;
